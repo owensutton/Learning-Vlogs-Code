630a877a3f19d024665e35ef8fb2c6d461881265
owensutton <45274107+owensutton@users.noreply.github.com>
2023-07-15 14:50:17 -0400

    java vlog2

---
+++ Java/Vlog2/VSUM.java
@@ -0,0 +1,15 @@
+package Java.Vlog2;
+
+// Vlog2 
+// to run with package java -cp . VSUM.java
+
+public class VSUM{
+    public static void main(String[]args){
+        int a = 123;
+        int b = 456;
+        int sum = 0;
+
+        sum = a + b;
+        System.out.println("Your Sum is " + sum);
+    }
+}
