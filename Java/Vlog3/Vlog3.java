0acaa0d962a77e3b218edf1ae48ab1cc37e46d97
owensutton <45274107+owensutton@users.noreply.github.com>
2023-07-31 20:04:44 -0400

    Java Vlog3

---
+++ Java/Vlog3/Vlog3.java
@@ -0,0 +1,25 @@
+package Java.Vlog3;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Vlog3 {
+    public static void main(String[]args){
+
+        char ch;
+        String newstring = "";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Input: ");
+        String theinput = input.nextLine();
+
+        System.out.println("Input word = " + theinput);
+
+        for (int i=0; i<theinput.length(); i++){
+            ch = theinput.charAt(i);
+            newstring = ch + newstring;
+        }
+        
+        System.out.println("Output word = " + newstring);
+
+    }
+}
