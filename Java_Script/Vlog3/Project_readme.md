4ff7b44cdd8949814b69824dbd0c0b53b7f69338
owensutton <45274107+owensutton@users.noreply.github.com>
2023-08-18 22:35:37 -0400

    Java_Scripts Updates

---
+++ Java_Script/Vlog3/Project_readme.md
@@ -0,0 +1,7 @@
+
+Building Flappy Bird within Java Script. 
+
+Steps
+
+1. Download P5 libraries. 
+
---
+++ Java_Script/Vlog3/background.js
@@ -0,0 +1,11 @@
+
+// Building the background 
+
+function setup() {
+    // Create canvas 
+    createCanvas(700, 700);
+}
+   
+function draw() {
+    background('blue');      
+}
