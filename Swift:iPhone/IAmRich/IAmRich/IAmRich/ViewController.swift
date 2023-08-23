52bd501eb0e6578fb360180626d37254976841a7
owensutton <45274107+owensutton@users.noreply.github.com>
2023-08-23 12:56:44 -0400

    Swift Vlog2

---
+++ Swift:iPhone/IAmRich/IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Owen Sutton on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
